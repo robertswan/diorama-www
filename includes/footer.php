2c38e7210ab00e22218405767c36f94940540744
Robert Swan <mammal.robert@gmail.com>
2016-09-30 12:20:58 +0100

    bottom menu options corrected links

--- includes/footer.php
+++ includes/footer.php
@@ -2,22 +2,19 @@
 
   <div id="foot-left" class="foot-bg">
     <nav id="foot-nav">
-      <a href="index.php"><li class="foot-link">Home</li></a>
-      <a href="index.php#download"><li class="foot-link">Download</li></a>
-      <a href="faq.htm"><li class="foot-link">FAQ</li></a>
-      <a href="devlog.htm"><li class="foot-link">Devlog</li></a>
-      <a href="media.htm"><li class="foot-link">Media &amp; Press</li></a>
-      <a href="contact.htm"><li class="foot-link">Contact</li></a>
+      <a href="index.php" class="foot-link">Home</a><br>
+      <a href="index.php#download" class="foot-link">Download</a><br>
+      <a href="faq.htm" class="foot-link">FAQ</a><br>
+      <a href="devlog.htm" class="foot-link">Devlog</a><br>
+      <a href="media.htm" class="foot-link">Media &amp; Press</a><br>
+      <a href="contact.htm" class="foot-link">Contact</a><br>
     </nav>
   </div>
 
   <div id="foot-right" class="foot-bg">
     <div id="social">
-
       <a href="http://twitch.tv/robtheswan"><div id="twitch" class="social-link"></div></a>
-
       <a href="http://twitter.com/robtheswandev"><div id="twitter" class="social-link"></div></a>
-
       <a href="http://youtube.com/c/dioramagame"><div id="youtube" class="social-link"></div></a>
     </div>
 
@@ -28,4 +25,6 @@
         <span id="copy">&copy; Diorama <?php echo date('Y', time()); ?>. All Rights Reserved.</span>
     </div>
   </div>
+
+  <div style="clear:both;"></div>
 </div>
